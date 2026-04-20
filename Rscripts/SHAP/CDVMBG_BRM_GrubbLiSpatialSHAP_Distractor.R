3d565e405f3fdb59db8bb638b6740591800da73d
nicholasdcrotty <nicholasdcrotty@gmail.com>
2026-04-20 12:21:51 -0400

    Further cleaned up commenting

--- Rscripts/SHAP/CDVMBG_BRM_GrubbLiSpatialSHAP_Distractor.R
+++ Rscripts/SHAP/CDVMBG_BRM_GrubbLiSpatialSHAP_Distractor.R
@@ -5,7 +5,7 @@ set.seed(1823) #for replication - the year Trinity College was founded!
 
 
 
-#replace the empty quotes with the file path to the location where you downloaded the files from Dropbox below
+#replace the empty quotes with the file path to the location where you downloaded the files from Zenodo below
 path = ''
 
 mode = "BS"
@@ -24,7 +24,7 @@ subsetCorrects = 1 #toggle for all trials vs. correct trials
 
 #----- Load in data and functions -----
 setwd(paste(path, "/dataForDownload/environments", sep = ""))
-source('CDVMBG_BRM_SpatialSHAPFunctions.R') #Separate script for KDE functions -- COMPLETE THIS
+source('CDVMBG_BRM_SpatialSHAPFunctions.R') #Separate script for KDE functions
 load("CDVMBG_BRM_DistPredictionsAndEyeSorted.RData")
 
 #----- Load in data -----
